6ae0ff5bbb5123efacdfabbb700708ac47a9b627
Felix Schütz <fschuetz04@gmail.com>
2014-11-11 22:12:54 +0000

    Update package.json; require aObject as a-object

--- configLoadr.js
+++ configLoadr.js
@@ -1,6 +1,6 @@
 var fs = require('fs'),
 	async = require('async'),
-	aObject = require('../aObject/aObject.js');
+	aObject = require('a-object');
 
 ConfigLoadr.globalNamespace = '$globalNamespace';
 ConfigLoadr.defaultEnvironment = '$defaultEnvironment';
