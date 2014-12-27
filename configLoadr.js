bb733b30e76255510cd543fc83260c0d514818a0
Felix Schütz <fschuetz04@gmail.com>
2014-12-27 00:54:16 +0000

    Bugfixes in configLoadr.js, use objectAnalyzr instead of aObject

--- configLoadr.js
+++ configLoadr.js
@@ -1,6 +1,6 @@
 var fs = require('fs'),
 	async = require('async'),
-	aObject = require('a-object'),
+	objectAnalyzr = require('object-analyzr'),
 	path = require('path');
 
 ConfigLoadr.globalNamespace = '$globalNamespace';
@@ -21,9 +21,7 @@ function ConfigLoadr() {
 		options = parsedArguments.options,
 		next = parsedArguments.callback,
 		_this = this;
-	if(typeof options.saveOptions == 'undefined' || options.saveOptions === true) {
-		this.options = options;
-	}
+	if(typeof options.saveOptions == 'undefined' || options.saveOptions === true) this.options = options;
 	this.globalConfig = {};
 	this.configNamespaces = {};
 	if(typeof options.syncConfig == 'undefined' || options.syncConfig === true) {
@@ -42,7 +40,8 @@ function ConfigLoadr() {
 			namespaces: this.configNamespaces
 		},
 		options,
-		function(error, config) {
+		function(err, config) {
+			if(err) return next(err);
 			next(null, {
 				global: _this.globalConfig,
 				namespaces: _this.configNamespaces
@@ -56,11 +55,10 @@ ConfigLoadr.prototype.load = function() {
 		load = parsedArguments.load,
 		options = parsedArguments.options,
 		next = parsedArguments.callback;
-	if(options.saveOptions === true) {
-		this.options = options;
-	}
+	if(options.saveOptions === true) this.options = options;
 	var _this = this;
-	loadConfig(load, {global: this.globalConfig, namespaces: this.configNamespaces}, options, function(error, config) {
+	loadConfig(load, {global: this.globalConfig, namespaces: this.configNamespaces}, options, function(err, config) {
+		if(err) return next(err);
 		next(null, {
 			global: _this.globalConfig,
 			namespaces: _this.configNamespaces
@@ -75,36 +73,26 @@ ConfigLoadr.prototype.setOptions = function() {
 
 ConfigLoadr.prototype.get = function(namespaces, includeGlobalConfig) {
 	var returnObject = {};
-	if(typeof includeGlobalConfig == 'undefined' || includeGlobalConfig) {
-		returnObject = this.globalConfig;
-	}
+	if(typeof includeGlobalConfig == 'undefined' || includeGlobalConfig) returnObject = this.globalConfig;
 	if(typeof namespaces != 'undefined') {
 		if(typeof namespaces == 'string') {
 			if(namespaces == ConfigLoadr.completeConfig) {
 				aObject.eachSync(this.configNamespaces, function(namespace, configNamespace) {
 					returnObject[key] = value;
 				});
-			} else {
-				returnObject[namespaces] = this.configNamespaces[namespaces];
-			}
+			} else returnObject[namespaces] = this.configNamespaces[namespaces];
 		} else if(typeof namespaces == 'object') {
 			namespaces.forEach(function(namespace, key) {
 				returnObject[namespace] = this.configNamespaces[namespace];
 			});
-		} else {
-			throw new TypeError('unsupported type of namespaces');
-		}
+		} else throw new TypeError('unsupported type of namespaces');
 	}
 	return returnObject;
 };
 
 function parseArguments(givenArguments, instanceOptions) {
-	if(givenArguments.length > 3) {
-		return new Error('too many arguments given');
-	}
-	if(typeof instanceOptions == 'undefined' || instanceOptions.resetOptions === true) {
-		instanceOptions = defaultOptions;
-	}
+	if(givenArguments.length > 3) return new Error('too many arguments given');
+	if(typeof instanceOptions == 'undefined' || instanceOptions.resetOptions === true) instanceOptions = defaultOptions;
 	var parsedArguments = {
 		load: [],
 		options: instanceOptions,
@@ -116,13 +104,10 @@ function parseArguments(givenArguments, instanceOptions) {
 				parsedArguments.callback = argument;
 				break;
 			case 'object':
-				if(Array.isArray(argument)) {
-					parsedArguments.load = argument;
-				} else {
+				if(Array.isArray(argument)) parsedArguments.load = argument;
+				else {
 					aObject.eachSync(parsedArguments.options, function(keyOption, value) {
-						if(typeof argument[keyOption] != 'undefined') {
-							parsedArguments.options[keyOption] = argument[keyOption];
-						}
+						if(typeof argument[keyOption] != 'undefined') parsedArguments.options[keyOption] = argument[keyOption];
 					});
 				}
 				break;
@@ -137,7 +122,8 @@ function loadConfig(load, config, options, next) {
 	if(typeof load == 'object') {
 		async.each(load,
 			function(file, nextFile) {
-				getConfigFile(file, options, function(error, configFile) {
+				getConfigFile(file, options, function(err, configFile) {
+					if(err) return nextFile(err);
 					updateConfig(
 						{
 							global: config.global,
@@ -149,7 +135,8 @@ function loadConfig(load, config, options, next) {
 					nextFile();
 				});
 			},
-			function(error) {
+			function(err) {
+				if(err) return next(err);
 				next(null, {
 					global: config.global,
 					namespaces: config.namespaces
@@ -162,15 +149,10 @@ function loadConfig(load, config, options, next) {
 }
 
 function updateConfig(currentConfig, newConfig, namespace) {
-	if(namespace == ConfigLoadr.globalNamespace) {
-		aObject.update(currentConfig.global, newConfig);
-	} else {
-		if(namespace == 'global') {
-			throw new Error('namespace global is not allowed');
-		}
-		if(typeof currentConfig.namespaces[namespace] == 'undefined') {
-			currentConfig.namespaces[namespace] = {};
-		}
+	if(namespace == ConfigLoadr.globalNamespace) aObject.update(currentConfig.global, newConfig);
+	else {
+		if(namespace == 'global') throw new Error('namespace global is not allowed');
+		if(typeof currentConfig.namespaces[namespace] == 'undefined') currentConfig.namespaces[namespace] = {};
 		aObject.update(currentConfig.namespaces[namespace], newConfig);
 	}
 }
@@ -178,33 +160,26 @@ function updateConfig(currentConfig, newConfig, namespace) {
 function getConfigFile(file, options, next) {
 	var noFileFound = true,
 		configFile = {};
-	if(typeof options.environments == 'string') {
-		options.environments = [options.environments];
-	}
+	if(typeof options.environments == 'string') options.environments = [options.environments];
 	async.each(options.environments,
 		function(environment, nextEnvironment) {
-			getConfigEnvironment(file, environment, options, function(error, configEnvironment) {
-				if(error) {
-					return nextEnvironment();
-				}
+			getConfigEnvironment(file, environment, options, function(err, configEnvironment) {
+				if(err) return nextEnvironment(err);
 				noFileFound = false;
 				aObject.update(configFile, configEnvironment);
 				nextEnvironment();
 			});
 		},
-		function(error) {
-			if(noFileFound) {
-				throw new Error('no environment files found for file: ' + file);
-			}
+		function(err) {
+			if(noFileFound) return next(new Error('no environment files found for file: ' + file));
+			if(err) return next(err);
 			next(null, configFile);
 		}
 	);
 }
 
 function getConfigEnvironment(file, environment, options, next) {
-	if(environment == ConfigLoadr.defaultEnvironment) {
-		environment = 'default';
-	}
+	if(environment == ConfigLoadr.defaultEnvironment) environment = 'default';
 	var environmentFile = options.base + '/' + options.configDirectory + '/';
 	switch(options.environmentStoreType) {
 		case 'extension':
@@ -220,18 +195,14 @@ function getConfigEnvironment(file, environment, options, next) {
 			environmentFile += file;
 			break;
 		default:
-			throw Error('unknown environmentStoreType: ' + options.environmentStoreType);
+			return next(new Error('unknown environmentStoreType: ' + options.environmentStoreType));
 	}
-	fs.readFile(environmentFile + '.json', {encoding: 'utf8'}, function(error, data) {
-		if (error) {
-			return next(error);
-		}
+	fs.readFile(environmentFile + '.json', {encoding: 'utf8'}, function(err, data) {
+		if (err) return next(err);
 		var configEnvironment = JSON.parse(data);
 		if(options.environmentStoreType == 'object') {
 			configEnvironment = configEnvironment[environment];
-			if (typeof configEnvironment == 'undefined') {
-				return next();
-			}
+			if (typeof configEnvironment == 'undefined') return next();
 		}
 		next(null, configEnvironment);
 	});
